86315f96a629e66652c916d70aa1fc1cf92cc17f
DhwaniBhavankarKarthikeyan <116158619+DhwaniBhavankarKarthikeyan@users.noreply.github.com>
2024-03-29 19:38:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import patients, alerts, contacts
 import pandas as pd
 
@@ -38,31 +37,14 @@ def home():
 
 class MultiApp:
     def __init__(self):
-        self.apps = []
-
-    def add_app(self, title, function):
-        self.apps.append({
-            "title": title,
-            "function": function
-        })
+        pass
 
     def run(self):
         if not login():
             return
 
-        with st.sidebar:
-            app = option_menu(
-                menu_title="MedWatcher",
-                options=['Home', 'Patients', 'Alerts', 'Contacts'],  # Removed 'Individual Patient Data' option
-                default_index=0,  # Set default index to 'Home'
-                styles={
-                    "container": {"padding": "5!important", "background-color": 'black'},
-                    "icon": {"color": "white", "font-size": "23px"},
-                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px",
-                                 "--hover-color": "blue"},
-                    "nav-link-selected": {"background-color": "#02ab21"},
-                }
-            )
+        st.sidebar.title("MedWatcher")
+        app = st.sidebar.radio("Navigation", ['Home', 'Patients'])
 
         if app == "Home":
             home()
