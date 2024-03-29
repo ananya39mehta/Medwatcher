48c9e597eac3a4841346f55a83dece78a773a6d6
DhwaniBhavankarKarthikeyan <116158619+DhwaniBhavankarKarthikeyan@users.noreply.github.com>
2024-03-30 01:03:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,35 +82,33 @@ def about_us():
     # Add your photo, name, and email ID
     st.write("## Meet the Team")
     
-    # Define a fixed height for each row
-    row_height = 150
-    
     # Create two columns for the photos
     col1, col2 = st.columns(2)
     
     # Photo 1
     with col1:
-        st.image("img1.jpeg", caption="Dhwani Bhavankar", width=100, height=row_height, use_column_width=False)
+        st.image("img1.jpeg", caption="Dhwani Bhavankar", width=100, use_column_width=False, output_format='png')
         st.write("**Dhwani Bhavankar**")
         st.write("dhwani.bhavankar.btech2022@sitpune.edu.in")
     
     # Photo 2
     with col2:
-        st.image("img2.jpeg", caption="Ananya Mehta", width=100, height=row_height, use_column_width=False)
+        st.image("img2.jpeg", caption="Ananya Mehta", width=100, use_column_width=False, output_format='png')
         st.write("**Ananya Mehta**")
         st.write("ananya39mehta@gmail.com")
 
     # Photo 3
     with col1:
-        st.image("img3.jpeg", caption="Harsimran Kaur", width=100, height=row_height, use_column_width=False)
+        st.image("img3.jpeg", caption="Harsimran Kaur", width=100, use_column_width=False, output_format='png')
         st.write("**Harsimran Kaur**")
         st.write("harsimrankaur2493@gmail.com")
     
     # Photo 4
     with col2:
-        st.image("img4.jpeg", caption="Mayank Sahai", width=100, height=row_height, use_column_width=False)
+        st.image("img4.jpeg", caption="Mayank Sahai", width=100, use_column_width=False, output_format='png')
         st.write("**Mayank Sahai**")
         st.write("smayank2412@gmail.com")
+
 
 class MultiApp:
     def __init__(self):
