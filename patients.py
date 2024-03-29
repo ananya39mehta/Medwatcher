221845992de177237c6678ba57efe4fe38195f57
DhwaniBhavankarKarthikeyan <116158619+DhwaniBhavankarKarthikeyan@users.noreply.github.com>
2024-03-29 20:04:03 +0530

    Update patients.py

--- patients.py
+++ patients.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 def patients():
     st.write("## Patients Page")
     patient_id = st.text_input("Enter Patient ID:")
     if patient_id:
         main_df = pd.read_csv("main.csv")
-        patient_details = main_df[main_df['Patient ID'] == int(patient_id)]
+        patient_details = main_df[main_df['patientid'] == int(patient_id)]
         if not patient_details.empty:
             st.write("### Patient Details:")
             st.write(patient_details)
@@ -18,8 +17,7 @@ def patients():
             st.write("### Glucose Data:")
             st.write(glucose_data)
             st.write("### Glucose Graph:")
-            plt.plot(glucose_data['Date'], glucose_data['Glucose'])
-            st.pyplot(plt)
+            st.line_chart(glucose_data.set_index('Date')['Glucose'])
 
         else:
             st.error("Patient ID not found.")
